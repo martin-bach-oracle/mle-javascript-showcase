4d890f0f7fc20e333b61c526c006c19f14078df7
martin bach <martin.b.bach@oracle.com>
2026-01-08 16:15:38 +0100

    chore: update container images to 26ai

--- 01_setup/database.sh
+++ 01_setup/database.sh
@@ -20,33 +20,21 @@ which docker && CONTAINER_RUNTIME=docker
 which podman && CONTAINER_RUNTIME=podman
 
 if [[ ${CONTAINER_RUNTIME} == none ]]; then
-    echo ERR: must either have docker or podman installed
+    echo ERR: must either have docker or podman installed or in the path
     exit 1
-fi
-
-# get the latest image version based on the processor's architecture
-# this should prevent you from pulling the wrong image
-ARCH=$(uname -p)
-IMAGEVERSION=undefined
-
-if [[ ${ARCH} == aarch64 || ${ARCH} == arm ]]; then
-    # still waiting for the 23.8 ARM image  to be released
-    IMAGEVERSION=23.7.0.0-arm64
-elif [[ ${ARCH} == aarch64 || ${ARCH} == arm ]]; then
-    IMAGEVERSION=23.8.0.0-amd64
 fi
 
 # start the database
 ${CONTAINER_RUNTIME} run \
 --rm \
 --name=mleshowcase \
---env ORACLE_PWD=secret \
+--env ORACLE_PASSWORD="${DB_PASSWORD}" \
 --detach \
 --publish 1521:1521 \
 --volume ./01_setup/init:/opt/oracle/scripts/startup \
-container-registry.oracle.com/database/free:${IMAGEVERSION}
+docker.io/gvenzl/oracle-free:23.26.0
 
 # show the container status, should be healthy
-sleep 5 && ${CONTAINER_RUNTIME} ps
+sleep 15 && ${CONTAINER_RUNTIME} ps
 
 echo INFO: DONE
--- 01_setup/init/01_demouser.sql
+++ 01_setup/init/01_demouser.sql
@@ -1,19 +1,14 @@
 alter session set container = freepdb1;
 
 prompt create the demouser account ...
-create user demouser identified by demouser;
-grant db_developer_role to demouser;
+drop user if exists demouser;
+create user demouser identified by demouser
+default tablespace users
+quota 100m on users;
 
-prompt JavaScript grants...
-grant execute on javascript to demouser;
-grant execute dynamic mle to demouser;
-
-prompt role grants...
+prompt role grants ...
 grant soda_app to demouser;
 grant db_developer_role to demouser;
 
 prompt default role all ...
 alter user demouser default role all;
-
-prompt quota ...
-alter user demouser quota 100m on users;
